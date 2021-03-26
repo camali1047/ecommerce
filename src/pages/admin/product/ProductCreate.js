bf18ec9628a4d7e07857ba62a84a8d72abb2806f
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-03-25 23:04:43 -0400

    modified Product

--- src/pages/admin/product/ProductCreate.js
+++ src/pages/admin/product/ProductCreate.js
@@ -3,15 +3,87 @@ import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { createProduct } from "../../../functions/product";
+import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getCategories, getCategorySubs } from "../../../functions/category";
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft", "Lenova", "Asus"],
+  color: "",
+  brand: "",
+};
 
 const ProductCreate = () => {
+  const [values, setValues] = useState(initialState);
+  const [subOptions, setSubOptions] = useState([]);
+  const [showSub, setShowSub] = useState(false);
+
+  //redux
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((c) => setValues({ ...values, categories: c.data }));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createProduct(values, user.token)
+      .then((res) => {
+        console.log(res);
+        window.alert(`${res.data.title} is created`);
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+        //if (err.response.status === 400) toast.error(err.response.data);
+        toast.error(err.response.data.err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleCategoryChange = (e) => {
+    e.preventDefault();
+    console.log("CLICKED CATEGORY", e.target.value);
+    setValues({ ...values, category: e.target.value });
+    getCategorySubs(e.target.value).then((res) => {
+      console.log("SUB OPTIONS ON CAREGORY CLICK", res);
+      setSubOptions(res.data);
+    });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col-md-10">product create form</div>
+        <div className="col-md-10">
+          <h4>Product Create</h4>
+          <hr />
+
+          <ProductCreateForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            values={values}
+            handleCategoryChange={handleCategoryChange}
+            subOptions={subOptions}
+            showSub={showSub}
+          />
+        </div>
       </div>
     </div>
   );
